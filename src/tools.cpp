e9906115a99951e7143a4e0657a9bfb6142a74c5
pstancato <paolo.stancato@fluxit.com.ar>
2017-04-16 03:18:36 -0300

    RMSE calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,25 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if (estimations.size() == 0) {
+    std::cout << "CalculateRMSE () - Error - no estimations" << std::endl;
+  }
+
+  if (estimations.size() != ground_truth.size()) {
+    std::cout << "CalculateRMSE () - Error - estimations and ground truth mismatch" << std::endl;
+  }
+
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd res = estimations[i] - ground_truth[i];
+    res = res.array() * res.array();
+    rmse += res;
+  }
+
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
