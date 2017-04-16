2be6a515aafea1dab7f33e97bbb895c5581b669d
pstancato <paolo.stancato@fluxit.com.ar>
2017-04-16 03:19:52 -0300

    Predict and LIDAR update steps

--- src/ukf.h
+++ src/ukf.h
@@ -17,6 +17,9 @@ public:
   ///* initially set to false, set to true in first call of ProcessMeasurement
   bool is_initialized_;
 
+  ///* previous timestamp
+  long previous_timestamp_;
+
   ///* if this is false, laser measurements will be ignored (except for init)
   bool use_laser_;
 
